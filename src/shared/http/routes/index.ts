18b59ecec8e59413f36598f71c9ed8dd20117287
Lucas de Souza <lsantos@fastprobr.com>
2022-06-03 16:09:23 -0300

    testando

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import userRouter from "@modules/users/routes/users.routes";
-import productsRouter from "@modules/producers/routes/products.routes";
+import productsRouter from "@modules/produtos/routes/products.routes";
 import sessionrRouter from "@modules/users/routes/sessions.routes";
 
 const routes = Router();
