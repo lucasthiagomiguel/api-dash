84078852037b33ab8ae317eead76b908f8d7a6c4
Lucas de Souza <lsantos@fastprobr.com>
2022-06-03 15:46:25 -0300

    arrumando arquivos

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import userRouter from "@modules/users/routes/users.routes";
 import productsRouter from "@modules/produtos/routes/products.routes";
-import categoryRouter from "@modules/categories/routes/category.routes";
 import sessionrRouter from "@modules/users/routes/sessions.routes";
 
 const routes = Router();
@@ -9,6 +8,5 @@ const routes = Router();
 routes.use('/users', userRouter);
 routes.use('/login', sessionrRouter);
 routes.use('/products', productsRouter);
-routes.use('/category', categoryRouter);
 
 export default routes;
